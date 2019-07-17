a381e77afefe588f311cf4631bc2d780562f7798
q269482074 <49773808+q269482074@users.noreply.github.com>
2019-07-17 13:10:18 +0800

    vue项目基本布置
    vue项目基本布置

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './routers'
+import store from './stores'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
+//引入mint-ui
+Vue.use(MintUI)
+
 
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
---
+++ src/views/Home/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <Header />
+            <br>
+            <br>
+            <br>
+            <div>123123</div>
+        <Bottom />
+    </div>
+</template>
+
+<script>
+
+import Header from '@/components/Header';
+import Bottom from '@/components/Bottom';
+
+
+export default {
+    name : 'Home',
+    components : {
+        Header,
+        Bottom
+    }
+}
+</script>
+
+
+
+<style>
+
+</style>
